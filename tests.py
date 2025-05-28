0d4e450dd610f03d4428eb874bf713848199fa8d
Hiroeme <83380500+Hiroeme@users.noreply.github.com>
2025-05-27 17:51:19 -0700

    added tests

---
+++ tests.py
@@ -0,0 +1,47 @@
+import unittest
+from maze import Maze
+
+class Test(unittest.TestCase):
+    def test_maze_create_cells_1(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._Maze__cells[0]),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._Maze__cells),
+            num_rows,
+        )
+
+    def test_maze_create_cells_2(self):
+        num_cols = 1
+        num_rows = 1
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._Maze__cells[0]),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._Maze__cells),
+            num_rows,
+        )
+
+    def test_maze_create_cells_3(self):
+        num_cols = 0
+        num_rows = 0
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._Maze__cells),
+            num_cols,
+        )
+
+        self.assertEqual(
+            len(m1._Maze__cells),
+            num_rows,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
